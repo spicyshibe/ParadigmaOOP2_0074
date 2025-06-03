587e473cce04b7d10ff067dd0a1b1d54aea61981
spicyshibe <rafierasyw@gmail.com>
2025-06-03 15:00:16 +0700

    class modifier public

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -6,3 +6,15 @@ class remoteLampu
     private:
         string saklarNo[10];
 
+    public:
+        void setSaklarNo(int i, string value)
+        {
+            saklarNo[i] = value;
+
+        }
+        string getSaklarNo(int i)
+        {
+            return saklarNo[i];
+        }
+};
+
