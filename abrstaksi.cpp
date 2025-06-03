6d7b034adecc12671c0cd38377a0d325814a2908
spicyshibe <rafierasyw@gmail.com>
2025-06-03 14:50:32 +0700

    abstraksi done

--- abrstaksi.cpp
+++ abrstaksi.cpp
@@ -7,4 +7,25 @@ class AbstraksiKlas {
     public: 
         //method untuk mengisi nilai 
         //private member
+        void setXY(string a, string b) {
+            x = a;
+            y = b;
+
+        }
+        //menampilkan nilai
+        void display ()
+        {
+            cout << "x = " << x << endl;
+            cout << "y = " << y << endl;
+
+        }
+};
+
+int main() {
+    AbstraksiKlas ak;
+    ak.setXY("yogyakarta", "kampus");
+    ak.display();
+
+    return 0;
+    
 }
